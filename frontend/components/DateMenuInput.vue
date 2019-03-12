21821706a4415bbe4bec2a9147ddb2b070adfaff
Julka Grodel <julka.grodel@framebridge.com>
2019-03-12 19:06:11 -0400

    Creates a date input component

---
+++ frontend/components/DateMenuInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-menu
+    v-model="showMenu"
+    :close-on-content-click="false"
+    :nudge-right="40"
+    lazy
+    transition="scale-transition"
+    offset-y
+    full-width
+    min-width="290px"
+  >
+    <template v-slot:activator="{ on }">
+      <v-text-field
+        :value="value"
+        :label="label"
+        prepend-icon="event"
+        readonly
+        v-on="on"
+        @input="setDate"
+      />
+    </template>
+    <v-date-picker
+      :value="value"
+      @input="setDate"
+    />
+  </v-menu>
+</template>
+
+<style>
+
+</style>
+
+<script>
+const component = {
+  props: {
+    value: String,
+    label: String
+  },
+  data () {
+    return {
+      showMenu: false
+    }
+  },
+  methods: {
+    setDate (newValue) {
+      this.showMenu = false;
+      this.$emit("input", newValue);
+    }
+  }
+}
+
+export default component
+</script>
