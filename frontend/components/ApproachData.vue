0c0a3d29433fe4f2a24c8eaac0375d428dcb8e86
Julka Grodel <julka.grodel@framebridge.com>
2019-03-12 22:16:58 -0400

    Adds approach data component

---
+++ frontend/components/ApproachData.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>{{ dateTime }}</p>
+    <p>{{ value.relative_velocity.miles_per_hour }}/mph</p>
+    <p>{{ value.miss_distance.miles }} miles from Earth</p>
+    <p>Orbiting body: {{ value.orbiting_body }}</p>
+  </div>
+</template>
+<script>
+
+const component = {
+  props: {
+    value: Object
+  },
+  computed: {
+    dateTime () {
+      return (new Date(this.value.epoch_date_close_approach)).toLocaleString()
+    }
+  }
+}
+
+export default component
+</script>
