8e1686018a111bb96b2f8d994c9b3fa3a7311f85
Julka Grodel <julka.grodel@framebridge.com>
2019-03-12 22:23:50 -0400

    Adds getNearestEarthObject to NasaApi mixin

--- frontend/mixins/NasaApi.js
+++ frontend/mixins/NasaApi.js
@@ -6,6 +6,39 @@ function getNasaApiSearchParams () {
   return searchParams
 }
 
+function findNearestAppoachingObject (neObjects) {
+  let output
+  for (const date in neObjects) {
+    output = neObjects[date].reduce(
+      (nearestSoFar, neObject) => {
+        const thisApproach = findNearestApproach(neObject)
+        const thatApproach = nearestSoFar && nearestSoFar.closest_approach_data
+        if (!nearestSoFar || isCloserApproach(thisApproach, thatApproach)) {
+          return { ...neObject, closest_approach_data: thisApproach }
+        } else {
+          return nearestSoFar
+        }
+      },
+      output
+    )
+  }
+  return output
+}
+
+function isCloserApproach (thisApproach, thatApproach) {
+  return parseInt(thisApproach.miss_distance.miles) < parseInt(thatApproach.miss_distance.miles)
+}
+
+function findNearestApproach (neObject) {
+  return neObject.close_approach_data.reduce((nearestSoFar, anotherApproach) => {
+    if (nearestSoFar.miss_distance.miles > anotherApproach.miss_distance.miles) {
+      return anotherApproach
+    } else {
+      return nearestSoFar
+    }
+  })
+}
+
 const mixin = {
   methods: {
     getNearEarthObjects (startDate, endDate) {
@@ -15,6 +48,11 @@ const mixin = {
       const getParams = searchParams.toString()
       const url = `https://api.nasa.gov/neo/rest/v1/feed?${getParams}`
       return fetch(url, { method: 'GET' }).then((response) => response.json())
+    },
+    getNearestEarthObject (startDate, endDate) {
+      return this.getNearEarthObjects(this.start, this.end).then((results) => {
+        return findNearestAppoachingObject(results.near_earth_objects)
+      })
     }
   }
 }
