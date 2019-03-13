706c2d37f0bba21c7a509f5f66f2d57cd2271987
Julka Grodel <julka.grodel@framebridge.com>
2019-03-13 02:05:18 -0400

    Preps user helper file for user authentication and does user document sanitization

--- backend/user.js
+++ backend/user.js
@@ -1,10 +1,13 @@
 const bcrypt = require('bcrypt')
+const uuid = require('node-uuid')
 const env = require('../env')
 
 const user = {}
 
 user.findByUsername = function (username, db) {
-  return user.find({ username }, db)
+  return findByUsernameUnsanitized(username, db).then((docs) => {
+    return docs.map(sanitize)
+  })
 }
 
 user.create = function (username, password, db) {
@@ -15,10 +18,45 @@ user.create = function (username, password, db) {
           username,
           salt,
           hash,
+          token: uuid(),
           created: Math.floor(Date.now() / 1000)
         },
         db
-      )
+      ).then(() => {
+        return user.authenticatePassword(username, password, db)
+      });
+    })
+  })
+}
+
+user.authenticatePassword = function (username, password, db) {
+  return new Promise((resolve, reject) => {
+    findByUsernameUnsanitized(username, db).then((docs) => {
+      if (!docs.length) {
+        reject(new Error('User does not exist'))
+      } else {
+        const userDoc = docs.pop()
+        user.generateHash(password, userDoc.salt).then((hash) => {
+          if (hash !== userDoc.hash) {
+            reject(new Error('Wrong password'))
+          } else {
+            resolve(sanitize(userDoc))
+          }
+        })
+      }
+    })
+  })
+}
+
+user.authenticateToken = function (username, token, db) {
+  return new Promise((resolve, reject) => {
+    find({ username, token }, db).then((docs) => {
+      if (docs.length) {
+        const userDoc = docs.pop()
+        resolve(sanitize(userDoc))
+      } else {
+        reject(new Error('User does not exist'))
+      }
     })
   })
 }
@@ -29,13 +67,13 @@ user.createDocument = function (userDoc, db) {
       if (error) {
         reject(error)
       } else {
-        resolve(result)
+        resolve(result.ops)
       }
     })
   })
 }
 
-user.generateSalt = function() {
+user.generateSalt = function () {
   return new Promise((resolve, reject) => {
     bcrypt.genSalt(env.saltRounds, (error, salt) => {
       if (error) {
@@ -47,7 +85,7 @@ user.generateSalt = function() {
   })
 }
 
-user.generateHash = function(password, salt) {
+user.generateHash = function (password, salt) {
   return new Promise((resolve, reject) => {
     bcrypt.hash(password, salt, (error, hash) => {
       if (error) {
@@ -63,7 +101,7 @@ user.collection = function (db) {
   return db.collection('users')
 }
 
-user.find = function (search, db) {
+function find (search, db) {
   return new Promise((resolve, reject) => {
     user.collection(db).find(search).toArray((error, result) => {
       if (error) {
@@ -75,4 +113,16 @@ user.find = function (search, db) {
   })
 }
 
-module.exports = user;
+function findByUsernameUnsanitized (username, db) {
+  return find({ username }, db);
+}
+
+function sanitize (userDoc) {
+  return {
+    username: userDoc.username,
+    created: userDoc.created,
+    token: userDoc.created
+  }
+}
+
+module.exports = user
