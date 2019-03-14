284139c724ee060b263aca8d84d0fdf6beb3c51a
Julka Grodel <julka.grodel@framebridge.com>
2019-03-13 20:53:07 -0400

    Changes auth for getAllForUser

--- frontend/mixins/Neo.js
+++ frontend/mixins/Neo.js
@@ -1,16 +1,16 @@
 const mixin = {
   methods: {
-    getAllForUser (username) {
-      const searchParams = new URLSearchParams()
-      searchParams.append('username', username)
-      const getParams = searchParams.toString()
-      const url = `http://localhost:3001/neos?${getParams}`
-
+    getAllForUser (username, token) {
+      const url = 'http://localhost:3001/neos'
       return fetch(
         url,
         {
           method: 'GET',
-          headers: { 'Content-Type': 'application/json' }
+          headers: {
+            'Content-Type': 'application/json',
+            'X-API-Key': token,
+            'X-API-ID': username
+          }
         }
       )
     }
