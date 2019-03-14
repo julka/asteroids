a6fc00d9bd0a8915eac3a640fafb90a033be64c5
Julka Grodel <julka.grodel@framebridge.com>
2019-03-13 22:44:27 -0400

    Refactors user to use more promises

--- backend/user.js
+++ backend/user.js
@@ -5,24 +5,22 @@ const env = require('../env')
 const user = {}
 
 user.findByUsername = function (username, db) {
-  return findByUsernameUnsanitized(username, db).then((docs) => {
-    return docs.map(sanitize)
+  return user.collection(db).findOne({ username }).then((userDoc) => {
+    return userDoc ? sanitize(userDoc) : userDoc
   })
 }
 
 user.create = function (username, password, db) {
-  return user.generateSalt().then((salt) => {
-    return user.generateHash(password, salt).then((hash) => {
-      return user.createDocument(
-        {
-          username,
-          salt,
-          hash,
-          token: uuid(),
-          created: Math.floor(Date.now() / 1000)
-        },
-        db
-      ).then(() => {
+  return bcrypt.genSalt(env.saltRounds).then((salt) => {
+    return bcrypt.hash(password, salt).then((hash) => {
+      const userDoc = {
+        username,
+        salt,
+        hash,
+        token: uuid(),
+        created: Math.floor(Date.now() / 1000)
+      }
+      return user.createDocument(userDoc, db).then(() => {
         return user.authenticatePassword(username, password, db)
       })
     })
@@ -30,70 +28,34 @@ user.create = function (username, password, db) {
 }
 
 user.authenticatePassword = function (username, password, db) {
-  return new Promise((resolve, reject) => {
-    findByUsernameUnsanitized(username, db).then((docs) => {
-      if (!docs.length) {
-        reject(new Error('User does not exist'))
-      } else {
-        const userDoc = docs.pop()
-        user.generateHash(password, userDoc.salt).then((hash) => {
-          if (hash !== userDoc.hash) {
-            reject(new Error('Wrong password'))
-          } else {
-            resolve(sanitize(userDoc))
-          }
-        })
-      }
-    })
+  return findByUsernameUnsanitized(username, db).then((userDoc) => {
+    if (!userDoc) {
+      return Promise.reject(new Error('User does not exist'))
+    } else {
+      return bcrypt.hash(password, userDoc.salt).then((hash) => {
+        if (hash !== userDoc.hash) {
+          return Promise.reject(new Error('Wrong password'))
+        } else {
+          return sanitize(userDoc)
+        }
+      })
+    }
   })
 }
 
 user.authenticateToken = function (username, token, db) {
-  return new Promise((resolve, reject) => {
-    find({ username, token }, db).then((docs) => {
-      if (docs.length) {
-        const userDoc = docs.pop()
-        resolve(sanitize(userDoc))
-      } else {
-        reject(new Error('Bad authentication'))
-      }
-    })
+  return user.collection(db).findOne({ username, token }).then((userDoc) => {
+    if (userDoc) {
+      return sanitize(userDoc)
+    } else {
+      return Promise.reject(new Error('Bad authentication'))
+    }
   })
 }
 
 user.createDocument = function (userDoc, db) {
-  return new Promise((resolve, reject) => {
-    return user.collection(db).insertOne(userDoc, (error, result) => {
-      if (error) {
-        reject(error)
-      } else {
-        resolve(result.ops)
-      }
-    })
-  })
-}
-
-user.generateSalt = function () {
-  return new Promise((resolve, reject) => {
-    bcrypt.genSalt(env.saltRounds, (error, salt) => {
-      if (error) {
-        reject(error)
-      } else {
-        resolve(salt)
-      }
-    })
-  })
-}
-
-user.generateHash = function (password, salt) {
-  return new Promise((resolve, reject) => {
-    bcrypt.hash(password, salt, (error, hash) => {
-      if (error) {
-        reject(error)
-      } else {
-        resolve(hash)
-      }
-    })
+  return user.collection(db).insertOne(userDoc).then((result) => {
+    return result.ops
   })
 }
 
@@ -101,20 +63,8 @@ user.collection = function (db) {
   return db.collection('users')
 }
 
-function find (search, db) {
-  return new Promise((resolve, reject) => {
-    user.collection(db).find(search).toArray((error, result) => {
-      if (error) {
-        reject(error)
-      } else {
-        resolve(result)
-      }
-    })
-  })
-}
-
 function findByUsernameUnsanitized (username, db) {
-  return find({ username }, db)
+  return user.collection(db).findOne({ username })
 }
 
 function sanitize (userDoc) {
