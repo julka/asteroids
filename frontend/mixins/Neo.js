0e71f117172b7ddf6e10e16641c8776c250b5e90
Julka Grodel <julka.grodel@framebridge.com>
2019-03-13 23:58:25 -0400

    Adds ui methods for create and update of notes

--- frontend/mixins/Neo.js
+++ frontend/mixins/Neo.js
@@ -13,6 +13,36 @@ const mixin = {
           }
         }
       )
+    },
+    addNeoNoteForUser (neoId, note, username, token) {
+      const url = `http://localhost:3001/neos/${neoId}`
+      return fetch(
+        url,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-API-Key': token,
+            'X-API-ID': username
+          },
+          body: JSON.stringify({note})
+        }
+      )
+    },
+    updateNeoNoteForUser(neoId, note, username, token) {
+      const url = `http://localhost:3001/neos/${neoId}`
+      return fetch(
+        url,
+        {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-API-Key': token,
+            'X-API-ID': username
+          },
+          body: JSON.stringify({note})
+        }
+      )
     }
   }
 }
